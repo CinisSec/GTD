56b44df1eb6059466502dbd2b8267448d60caf1f
grey <gregorypro@protonmail.com>
2024-05-16 22:49:16 +0200

    updating commands in gtd.py

--- gtd.py
+++ gtd.py
@@ -1,10 +1,10 @@
 import sqlite3
 import sys
-from typing import List, Optional
 # import plugins
 import plugins.capture as capture
-import plugins.organize as organize
-#import plugins.review as review
+import plugins.organize.organize as organize
+import plugins.organize.organizeprint as oprint
+import plugins.organize.organizemark as omark
 
 # connect to SQLite database
 conn = sqlite3.connect('gtd.db')
@@ -19,8 +19,6 @@ c.execute('''CREATE TABLE if not exists captures (
     idea TEXT not null,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     archived INTEGER default 0)''')
-
-
 
 # tasks
 # id | name        | description | due_date | part_of | depends_on | created        | completed | completed_date
@@ -43,26 +41,46 @@ c.execute('''CREATE TABLE if not exists tasks (
 conn.commit()
 conn.close()
 
+def overview():
+    print("CAPTURES")
+    capture.print_captures_active()
+    print("\nTASKS")
+    oprint.print_tasks()
+
 COMMANDS = {
-    
+    ## overview
+    "-o": (overview, 0),
     ## capture
     "-c": (capture.capture, 2), # idea
     "-cl": (capture.print_captures, 0),
     "-cla": (capture.print_captures_active, 0),
     "-ca": (capture.archive_capture, 2), # id
     "-cd": (capture.delete_capture, 2), # id
-
     ## organize
-    "-t": (organize.new_task, 5), # name, description, due_date, part_of
-    "-tnd": (organize.new_task_no_date, 4), # name, description, part_of
-    "-tco": (organize.complete_task, 2), # id
-    "-tdw": (organize.print_tasks_due_this_week, 0),
-    "-tdm": (organize.print_tasks_due_this_month, 0),
-    "-tl": (organize.print_tasks, 0),
-    "-tla": (organize.print_tasks_active, 0),
-    "-tlc": (organize.print_tasks_completed, 0),
-    "-tld": (organize.print_dependent_tasks, 0),
+    ### task creation
+    "-tcd": (organize.new_task, 5), # name, description, due_date, part_of
+    "-tc": (organize.new_task_no_date, 4), # name, description, part_of
+    ### task marking
+    "-tmc": (omark.mark_task_complete, 2), # id
+    "-tmi": (omark.mark_task_incomplete, 2), # id
+    #"-tmar": (omark.mark_tasks_archive, 2), # id
+    #### Task range marking
+    #"-tmcr": (omark.mark_task_complete_range, 2), # start_id, end_id
+    #"-tmir": (omark.mark_task_incomplete_range, 2), # start_id, end_id
+    #"-tmar": (omark.mark_tasks_archive_range, 2), # start_id, end_id
+    ### task deletion
+    "-td": (organize.delete_task, 2), # id
+    #"-tdr": (organize.delete_task_range, 2), # start_id, end_id
+    ### task printing
+    "-tdw": (oprint.print_tasks_due_this_week, 0),
+    "-tdm": (oprint.print_tasks_due_this_month, 0),
+    "-tl": (oprint.print_tasks, 0),
+    "-tla": (oprint.print_tasks_active, 0),
+    "-tlc": (oprint.print_tasks_completed, 0),
+    "-tld": (oprint.print_dependent_tasks, 0),
 }
+
+
 
 def main():
     if len(sys.argv) < 2:
